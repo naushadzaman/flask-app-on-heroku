75769d8134a3c59b9e9da1b1ff4bf8264e4942cb
Naushad UzZaman <naushadzaman@gmail.com>
2014-02-27 05:20:16 +0000

    update

--- app/views.py
+++ app/views.py
@@ -6,6 +6,18 @@ from flask import render_template
 
 def index(): 
 	user = { 'nickname': 'Naushad' } # fake user
+	posts = [ # fake array of posts
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
 	return render_template("index.html",
         title = 'Home',
-        user = user)
+        user = user,
+        posts = posts)
+
