2c3a6127fc8c77fa9e2dbea8160e6a195981d052
Naushad UzZaman <naushadzaman@gmail.com>
2014-03-01 05:37:36 +0000

    added results in search result; input from form and output

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect
 from app import app
 from forms import LoginForm
+from forms import InputLocation
 
 @app.route('/')
 @app.route('/index')
@@ -31,4 +32,35 @@ def login():
         title = 'Sign In',
         form = form,
         providers = app.config['OPENID_PROVIDERS'])
+
+
+@app.route('/input', methods = ['GET', 'POST'])
+def input():
+	form = InputLocation()
+	results = [
+        {
+            'name': 'Weber State University',
+            'rating': '9.0',
+	    'url': 'http://www.yelp.com/biz/weber-state-university-ogden-4',
+	    'photo': 'http://s3-media1.ak.yelpcdn.com/bphoto/0bzEERwyYnp5GTgyC56kqg/l.jpg'
+        },
+        {
+            'name': 'Two Creek Coffee House',
+            'rating': '8.9',
+            'url': 'http://www.yelp.com/biz/two-creek-coffee-house-bountiful',
+            'photo': 'http://s3-media1.ak.yelpcdn.com/bphoto/0bzEERwyYnp5GTgyC56kqg/l.jpg'
+        }
+    ]
+
+	if form.validate_on_submit():
+		flash('Zipcode: ' + form.zipcode.data)
+		return render_template('results.html', 
+			title = 'Entered Location', 
+			results = results)
+	return render_template('input.html',
+		title = 'Enter Location',
+		form = form)
+
+
+
 
